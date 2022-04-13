5a5cd0aa5510d06fa468a2e6d475b3c603a02487
Dev deRev <devderev@free.fr>
2022-04-14 00:36:53 +0200

    fix Order model

--- Iron-Bike-backend/models/Order.model.js
+++ Iron-Bike-backend/models/Order.model.js
@@ -1,22 +1,31 @@
 const { Schema, model } = require("mongoose");
 
-const OrderSchema = new Schema({
-    cartId:{type: Schema.Types.ObjectId, ref:'Cart'},
-    userId:{type: Schema.Types.ObjectId, ref:'User'},
-    status:{type:String, default:'PENDING', require:true},
-    fullname:{type:String, require:true},
-    address:{
-        streetNumber: { type: String },
-        streetName: { type: String, require:true},
-        postalCode: { type: String, require:true },
-        city: { type: String, require:true},
-        country: { type: String, require:true }
+const OrderSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+    cartId: { type: Schema.Types.ObjectId, ref: "Cart" },
+    paymentStatus: { type: String, default: "PENDING", require: true },
+    totalItems: Number,
+    totalAmount: Number,
+    type: String,
+    shipTo: {
+        fullname: { type: String, require: true },
+        address: {
+            streetNumber: { type: String },
+            streetName: { type: String, require: true },
+            postalCode: { type: String, require: true },
+            city: { type: String, require: true },
+            country: { type: String, require: true },
+        },
     },
-    totalAmount:Number,
-    delivered:{default:false}
+    orderStatus: {
+      enum: ["process", "shipped", "delivered", "canceled", "refund"],
+    },
   },
-  {timestamps:true}
-  );
+  {
+    timestamps: true,
+  }
+);
 
 const Order = model("Order", cartItemSchema);
 
