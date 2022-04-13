5eda7c55370b6856b98176a46f5044171faf9937
Léo Cheffi <contact@leocheffi.com>
2022-04-13 11:39:10 +0200

    add the products model and make the seed

--- Iron-Bike-backend/models/Products.model.js
+++ Iron-Bike-backend/models/Products.model.js
@@ -1,39 +1,111 @@
 const { Schema, model } = require("mongoose");
 
-const productsSchema = new Schema(
-  {
-    productName: {
-      type: String,
-      required: true,
-    },
-    brand: {
-        type: Schema.Types.ObjectId, 
-        ref: 'Brand',
-    },
-    color: {
-        type: String,
-    },
-    quantity: {
-        type: Number,
-    },
-    category: {
-        type: String,
-        enum: ["Road Bike","Mountain Bike","City Bike", "E-Bike", "Low Rider", "E-Accessories", "Accessories", "Helmets", "Glasses", "Jerseys", "Trousers", "Shoes","Nutrition"],
-    },
-    Characteristics :{
-        type: String,
-    },
-    weight :  Number,
-    size : {
-        type: String,
-        enum: [ "XXS", "XS", "S", "M", "L", "XL", "XXL"],
-    },
-    image : String,
-    price : Number,
-  }
-);
+const productsSchema = new Schema({
+  productName: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: Schema.Types.ObjectId,
+    ref: "Brand",
+  },
+  quantity: {
+    type: Number,
+  },
+  image: String,
+  price: Number,
+});
 
 const Products = model("Products", productsSchema);
 
-module.exports = Products;
-  
+const bikeSchema = new Schema({
+  categoryBike: {
+    type: String,
+    enum: ["Road Bike", "Mountain Bike", "City Bike", "E-Bike", "Low Rider"],
+  },
+  size: {
+    type: String,
+    enum: ["S", "M", "L"],
+  },
+  frameMaterials: {
+    type: String,
+    enum: ["aluminum", "carbon", "titanium"],
+  },
+  equipment: {
+    type: String,
+    enum: ["Shimano", "Campagnolo", "Sram"],
+  },
+  color: {
+    type: String,
+  },
+  weight: Number,
+});
+
+const Bike = Products.discriminator("Bike", bikeSchema);
+
+const accessoriesSchema = new Schema({
+  features: {
+    type: String,
+    enum: ["GPS", "Bluetooth", "Heart Rate Monitor", "Wireless"],
+  },
+});
+
+const Accessories = Products.discriminator("Accessories", accessoriesSchema);
+
+const nutritionSchema = new Schema({
+  aspect: {
+    type: String,
+    enum: ["Bars", "Gels", "Drinks", "Energy Meals", "Food Supplements"],
+  },
+  flavor: {
+    type: String,
+    enum: [
+      "Chocolate",
+      "Banana",
+      "Vanilla",
+      "Cookies",
+      "Peanut",
+      "Summer Fruits",
+      "Mango",
+    ],
+  },
+  toTake: {
+    type: String,
+    enum: ["Before Training", "During Training", "After Training"],
+  },
+  vegan: {
+    type: Boolean,
+    default: false,
+  },
+  weight: Number,
+});
+const Nutrition = Products.discriminator("Nutrition", nutritionSchema);
+
+const clothesSchema = new Schema({
+  sexCategory: {
+    type: String,
+    enum: ["Men", "Women", "Children"],
+  },
+  type: {
+    type: String,
+    enum: ["Helmets", "Jerseys", "Trousers", "Shoes", "Glasses"],
+  },
+  size: {
+    type: String,
+    enum: ["XXS", "XS", "S", "M", "L", "XL", "XXL"],
+  },
+  ReflectiveEquipment: {
+    type: String,
+    enum: ["yes", "None"],
+  },
+  rainProtection: {
+    type: String,
+    enum: ["yes", "None"],
+  },
+  color: {
+    type: String,
+  },
+});
+const Clothes = Products.discriminator("Clothes", clothesSchema);
+
+module.exports = { Products, Bike, Accessories, Nutrition, Clothes };
