741e57a2dd3ecf936475590b5efbd8967c15fda5
gary-j <gary.j@live.fr>
2022-04-20 04:35:18 +0200

    auth working!

--- Iron-Bike-backend/error-handling/index.js
+++ Iron-Bike-backend/error-handling/index.js
@@ -1,29 +1,38 @@
 module.exports = (app) => {
   app.use((req, res, next) => {
     // this middleware runs whenever requested page is not available
-    res.status(404).json({ errorMessage: "This route does not exist" });
+    res.status(404).json({ errorMessage: 'This route does not exist' });
   });
 
   app.use((err, req, res, next) => {
-
     // whenever you call next(err), this middleware will handle the error
     // always logs the error
-    
+
     console.error(
-      "ERROR",
-      "METHOD: ",
+      'ERROR',
+      'METHOD: ',
       req.method,
-      " PATH: ",
+      ' PATH: ',
       req.path,
-      "error: ",
+      'error: ',
       err
     );
+    console.log(Object.keys(err), ' les clefs de lerreur');
+
+    //jwt errors
+    if (err.code === 'invalid_token') {
+      console.log('invalid token');
+      res
+        .status(401)
+        // .res.redirect('/auth/login')
+        .json('Invalid token.Please Loggin');
+    }
 
     // only render if the error ocurred before sending the response
     if (!res.headersSent) {
       // res.redirect("/api");
       res.status(500).json({
-        errorMessage: "Internal server error. Check the server console",
+        errorMessage: 'Internal server error. Check the server console',
       });
     }
   });
