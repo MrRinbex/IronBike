ac941bef1b7961dfd52d967c170c524d7ad392f5
gary-j <gary.j@live.fr>
2022-04-21 19:52:44 +0200

    stripe route

--- Iron-Bike-backend/routes/stripe.routes.js
+++ Iron-Bike-backend/routes/stripe.routes.js
@@ -1,27 +1,26 @@
-const router = require("express").Router();
-const KEY = process.env.STRIPE_KEY
-const stripe = require("stripe")(KEY);
+const router = require('express').Router();
+const KEY = process.env.STRIPE_KEY;
+const stripe = require('stripe')(KEY);
+const { isAuthenticated } = require('../middleware/jwt.middleware');
 
-router.post("/payment", (req, res) => {
+router.post('/payment', (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: 'eur',
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        console.log(stripeErr, 'stripe response erreur');
 
-    stripe.charges.create(
-      {
-        source: req.body.tokenId,
-        amount: req.body.amount,
-        currency: "eur",
-      },
-      (stripeErr, stripeRes) => {
-        if (stripeErr) {
-          console.log(stripeErr, 'stripe response')
-
-          res.status(500).json(stripeErr);
-        } else {
-          console.log(stripeRes, 'stripe response')
-          res.status(200).json(stripeRes);
-        }
+        res.status(500).json(stripeErr);
+      } else {
+        console.log(stripeRes, 'stripe response ok');
+        res.status(200).json(stripeRes);
       }
-    );
-
+    }
+  );
 });
 
 module.exports = router;
